b177414cac75262b92a8b49542ff1c5decf9d0c0
Jejis06 <wertex3233@gmail.com>
2023-07-13 12:57:51 +0200

    statystyki

--- stats.py
+++ stats.py
@@ -1,9 +1,64 @@
 import json
 import matplotlib.pyplot as plt
-import numpy as np
 
-y = np.array([35, 25, 25, 15])
-mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
 
-plt.pie(y, labels = mylabels)
+
+names = []
+Pwybory = []
+Nwybory = []
+
+schools = []
+
+with open("db.json","rb") as f:
+    data = json.load(f)
+    f.close()
+
+
+cpw = 0
+cnw = 0
+for school in data:
+    names.append(school["name"])
+    pw = 0
+    nw = 0
+    for Class in school["classes"]:
+        pw += Class["Pwybor"]
+        nw += Class["Nwybor"]
+    Pwybory.append(pw)
+    Nwybory.append(nw)
+
+    cpw += pw
+    cnw += nw
+
+    schools.append({
+        'name':school['name'],
+        'pw':pw,
+        'nw':nw,
+    })
+
+
+schools = sorted(schools, key = lambda x : -x['pw'])
+
+print(f"kandydujacych : {cpw} :\n\n\n")
+j = 1
+print("\t\t\t\tStatystyki Pierwszy wybór : ") 
+print()
+for i in schools:
+    print(f"{j}\t{i['name']:120s}{(i['pw']/cpw):%}")
+    j += 1
+
+j = 1
+schools = sorted(schools, key = lambda x : -x['nw'])
+print("\n\n\n\n\t\t\t\tStatystyki kolejne wybory : ") 
+print()
+for i in schools:
+    print(f"{j}\t{i['name']:120s}{(i['nw']/cnw):%}")
+    j += 1
+        
+
+
+    
+
+plt.pie(Pwybory, labels = names, autopct='%1.1f%%',  pctdistance=1.1, labeldistance=1.2)
+plt.show()
+plt.pie(Nwybory, labels = names, autopct='%1.1f%%',  pctdistance=1.1, labeldistance=1.2)
 plt.show()
